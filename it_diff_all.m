eaaf4834051cab271ecc756e50c1d3d6683db4e5
michael-suntc <mike.suntc@gmail.com>
2017-12-07 23:22:44 -0800

    2

--- it_diff_all.m
+++ it_diff_all.m
@@ -10,9 +10,10 @@ diff_mean_pixel = [];
 case_id = {};
 
 show_figs = 1;
-mean_w = 1; % smoothing window size
+mean_w = 3; % smoothing window size
 base_channel = 11; % use channel 2's image for registration
-
+erode_radius = 4;
+        
 % get list of input files (suppose each pair of .mat file is in a seperate folder)
 current_folders = dir();
 current_folders = current_folders([current_folders.isdir]);
@@ -37,7 +38,7 @@ for i = 1 : length(current_folders) % for each folder
     end
     
     % variables for this image pair
-    diffs = []; % store difference value of each channel, as 1D array
+    diffs = {}; % store difference value of each channel, as 1D array
     outfile = fopen(strcat('stats-',current_folders(i).name, '.csv'), 'wt');
     fprintf(outfile, [',', 'mean,', 'median,', 'std,', 'min,', 'max,\n']);
     rawrun_1 = cell2mat(img_pair(1));
@@ -57,20 +58,32 @@ for i = 1 : length(current_folders) % for each folder
     
     % choose the channel (3rd dimension of AllData)
     for channel = 9 : 12
-    
+        % plot 4 channels in one figure
         % process image pair
-        run_1 = rawrun_1(:,:,channel);
-        run_2 = rawrun_2(:,:,channel);
-        run_1(run_1 < 1) = 1;
-        run_1(run_1 > 12) = 12;
-        run_2(run_2 < 1) = 1;
-        run_2(run_2 > 12) = 12;
-        
-        % mask, filter values < 15, replace with 1
+        run_1_orig = rawrun_1(:,:,channel);
+        run_2_orig = rawrun_2(:,:,channel);
+        run_1 = run_1_orig;
+        run_2 = run_2_orig;
+        % mask, filter values < 15
         snr_1 = rawrun_1(:,:,channel-4);
         snr_2 = rawrun_2(:,:,channel-4);
-        run_1(snr_1<15) = 1;
-        run_2(snr_2<15) = 1;
+        
+        % dilate / erode
+        noise_mask_1 = (run_1_orig < 1) | (run_1_orig > 12) | (snr_1 < 15);
+        noise_mask_2 = (run_2_orig < 1) | (run_2_orig > 12) | (snr_2 < 15);
+        se = strel('disk',erode_radius);
+        noise_mask_1 = imdilate(noise_mask_1, se);
+        noise_mask_2 = imdilate(noise_mask_2, se);
+        %noise_mask_1 = imerode(noise_mask_1, se);
+        %noise_mask_2 = imerode(noise_mask_2, se);
+        
+        % displaye noise mask
+        %figure, imagesc(noise_mask_1);
+        %figure, imagesc(noise_mask_2);
+        
+        % apply mask to image
+        run_1(noise_mask_1) = -1;
+        run_2(noise_mask_2) = -1;
         
         % register run2 to run_1
         try
@@ -97,11 +110,25 @@ for i = 1 : length(current_folders) % for each folder
         while isnan(sum(run_1(:,end)))
             run_1 = run_1(:,1:end-1);
         end
+        if size(run_1, 2) > size(run_2_res, 2)
+            run_1 = run_1(:,1:size(run_2_res, 2));
+        else
+            run_2_res = run_2_res(:,1:size(run_1, 2));
+        end
+        run_1(run_1 == -1) = NaN;
+        run_2_res(run_2_res == -1) = NaN;
+        noise_filter = isnan(run_2_res) | isnan(run_1);
+        run_1(isnan(run_1)) = NaN;
+        run_2_res(isnan(run_2_res)) = NaN;
         
         % smooth using mean
         h = ones(mean_w, mean_w) / (mean_w * mean_w);
         run_1 = imfilter(run_1, h);
         run_2_res = imfilter(run_2_res, h);
+        
+        % filter noise
+        run_1(noise_filter) = NaN;
+        run_2_res(noise_filter) = NaN;
 
         % subtract
         if (size(run_1,2) > size(run_2_res, 2))
@@ -110,6 +137,7 @@ for i = 1 : length(current_folders) % for each folder
             run_2_res = run_2_res(:, 1:size(run_1, 2));
         end
         diff_img = imabsdiff(run_1, run_2_res);
+        diff_img(isnan(diff_img)) = 0;
 
         % sum of difference
         %diff_matrices = [diff_matrices, diff_img];
@@ -129,47 +157,64 @@ for i = 1 : length(current_folders) % for each folder
         
         % show images
         if show_figs
-            %limit = [1, 12];
+            limit = [1, 12];
+            %{
             if max(run_1(:)) > max(run_2(:))
                 limit = [min(run_1(:)) max(run_1(:))];
             else
                 limit = [min(run_2(:)) max(run_2(:))];
             end
-
+            %}
             f = figure('visible', 'off');
             imagesc(run_1);
             colormap(jet), caxis(limit), colorbar;
+            set(gcf, 'Units', 'Inches', 'Position', [0, 0, 4.5, 7.5], 'PaperUnits', 'Inches', 'PaperSize', [4.5, 7.5]);
             saveas(f, fullfile('./', current_folders(i).name, strcat('1-',int2str(channel)) ), 'jpeg');
-
+            saveas(f, fullfile('./', current_folders(i).name, strcat('1-',int2str(channel), '.fig') ));
             f = figure('visible', 'off');
             imagesc(run_2_res);
             colormap(jet), caxis(limit), colorbar;
+            set(gcf, 'Units', 'Inches', 'Position', [0, 0, 4.5, 7.5], 'PaperUnits', 'Inches', 'PaperSize', [4.5, 7.5]);
             saveas(f, fullfile('./', current_folders(i).name, strcat('2-',int2str(channel)) ), 'jpeg');
+            saveas(f, fullfile('./', current_folders(i).name, strcat('2-',int2str(channel), '.fig') ));
 
             f = figure('visible', 'off');
             imagesc(diff_img);
-            colormap(flipud(gray)), caxis(limit), colorbar;
+            %colormap(flipud(gray)), caxis(limit), colorbar;
+            c = ceil(max(diff_img(:)));
+            if c < 2
+                c = 2;
+            end
+            colormap(jet), caxis([0, c]), colorbar;
+            set(gcf, 'Units', 'Inches', 'Position', [0, 0, 4.5, 7.5], 'PaperUnits', 'Inches', 'PaperSize', [4.5, 7.5]);
             saveas(f, fullfile('./', current_folders(i).name, strcat('diff-',int2str(channel)) ), 'jpeg');
+            saveas(f, fullfile('./', current_folders(i).name, strcat('diff-',int2str(channel), '.fig') ));
         end
     end
    % stats of this pair in all channel
-    pair_mean = mean(diffs(:));
-    pair_med = median(diffs(:));
-    pair_std = std(diffs(:));
-    pair_min = min(diffs(:));
-    pair_max = max(diffs(:));
+    diffs_1d = [];
+    for i = 1 : size(diffs, 2)
+        d = cell2mat(diffs(i));
+        diffs_1d = cat(1, diffs_1d, d(:));
+    end
+    pair_mean = mean(diffs_1d(:));
+    pair_med = median(diffs_1d(:));
+    pair_std = std(diffs_1d(:));
+    pair_min = min(diffs_1d(:));
+    pair_max = max(diffs_1d(:));
     fprintf(outfile, 'AllChannels, %f, %f, %f, %f, %f,\n', [pair_mean pair_med pair_std pair_min pair_max]');
     % exclude CH4
-    diffs_1_3 = diffs(:, 1:3);
-    pair_1_3_mean = mean(diffs_1_3(:));
-    pair_1_3_med = median(diffs_1_3(:));
-    pair_1_3_std = std(diffs_1_3(:));
-    pair_1_3_min = min(diffs_1_3(:));
-    pair_1_3_max = max(diffs_1_3(:));
+    count_1_3 = size(cell2mat(diffs(1)), 1) + size(cell2mat(diffs(2)), 1) + size(cell2mat(diffs(3)), 1);
+    diffs_1_3 = diffs_1d(1:count_1_3, :);
+    pair_1_3_mean = mean(diffs_1_3);
+    pair_1_3_med = median(diffs_1_3);
+    pair_1_3_std = std(diffs_1_3);
+    pair_1_3_min = min(diffs_1_3);
+    pair_1_3_max = max(diffs_1_3);
     fprintf(outfile, 'AllExcludingCH4, %f, %f, %f, %f, %f,\n', [pair_1_3_mean pair_1_3_med pair_1_3_std pair_1_3_min pair_1_3_max]');
     fclose(outfile);
     % add to difflist
-    diff_list = [diff_list, diffs];
+    diff_list = [diff_list, {diffs}];
 
 end
 
@@ -177,10 +222,16 @@ end
 diffs_all = [];
 diffs_all_1_3 = [];
 for i = 1 : size(diff_list, 2)
-    t = cell2mat(diff_list(i));
-    t_1_3 = t(:, 1:3);
-    diffs_all = cat(1, diffs_all, t(:));
-    diffs_all_1_3 = cat(1, diffs_all_1_3, t_1_3(:));
+    diffs = diff_list{1, i};
+    diffs_1d = [];
+    for j = 1 : size(diffs, 2)
+        d = cell2mat(diffs(j));
+        diffs_1d = cat(1, diffs_1d, d(:));
+    end
+    count_1_3 = size(cell2mat(diffs(1)), 1) + size(cell2mat(diffs(2)), 1) + size(cell2mat(diffs(3)), 1);
+    diffs_1_3 = diffs_1d(1:count_1_3, :);
+    diffs_all = cat(1, diffs_all, diffs_1d(:));
+    diffs_all_1_3 = cat(1, diffs_all_1_3, diffs_1_3(:));
 end
 outfile = fopen(strcat('stats-allcases', '.csv'), 'wt');
 fprintf(outfile, [',', 'mean,', 'median,', 'std,', 'min,', 'max,\n']);
@@ -188,4 +239,4 @@ fprintf(outfile, 'AllCases, %f, %f, %f, %f, %f,\n', [mean(diffs_all) median(diff
 fprintf(outfile, 'AllExcludingCH4, %f, %f, %f, %f, %f,\n', [mean(diffs_all_1_3) median(diffs_all_1_3) std(diffs_all_1_3) min(diffs_all_1_3) max(diffs_all_1_3)]');
 fclose(outfile);
 save('diff_list.mat', 'diff_list');
-save('case_id.mat', 'case_id');
+    save('case_id.mat', 'case_id');
--- stats_figures.m
+++ stats_figures.m
@@ -3,10 +3,10 @@ load('case_id.mat');
 
 % for each case
 for i = 1 : size(diff_list, 2)
-diff_case = cell2mat(diff_list(i));
+diff_case = diff_list{1, i};
 diff_case_all = [];
 for j = 1 : 4 % for each channel
-    diff_case_channel = diff_case(:, j);
+    diff_case_channel = cell2mat(diff_case(j));
     diff_case_all = cat(1, diff_case_all, diff_case_channel);
     % histogram
     title_str = strcat('Difference distribution: Case ', case_id(i), ' CH', int2str(j));
@@ -16,6 +16,7 @@ for j = 1 : 4 % for each channel
     xlim([0 6.5]) % choose 6.5 because the biggest difference in all cases/channels < 6.5
     % save image
     fname = char(strcat('Case', case_id(i),'-distribution', '-CH', int2str(j)));
+    set(gcf, 'Units', 'Inches', 'Position', [0, 0, 4.5, 7.5], 'PaperUnits', 'Inches', 'PaperSize', [4.5, 7.5]);
     saveas(f, fname, 'jpeg');
 end
 % box plot
@@ -26,8 +27,8 @@ end
 % here we choose w as 3 (extreme Outliers)
 f = figure('visible', 'off');
 x_labels = {'CH1', 'CH2', 'CH3', 'CH4'};
-x = cat(1, diff_case(:), diff_case_all);
-g = [ones(size(diff_case(:,1))); 2*ones(size(diff_case(:,2))); 3*ones(size(diff_case(:,3))); 4*ones(size(diff_case(:,4))); 5*ones(1, size(diff_case_all,1))'];
+x = cat(1, diff_case_all, diff_case_all);
+g = [ones(1, size(cell2mat(diff_case(:,1)) ,1) ), 2*ones(1, size(cell2mat(diff_case(:,2)) ,1) ), 3*ones(1, size(cell2mat(diff_case(:,3)) ,1) ), 4*ones(1, size(cell2mat(diff_case(:,4)) ,1) ), 5*ones(1, size(diff_case_all,1))];
 gcell = num2cell(g);
 gcell(g==1) = {'CH1'};
 gcell(g==2) = {'CH2'};
@@ -38,6 +39,7 @@ boxplot(x, gcell, 'Whisker', 3, 'OutlierSize',5);
 title_str = strcat('Box plot: Case ', case_id(i));
 title(title_str);
 fname = char(strcat('Case', case_id(i),'-box'));
+set(gcf, 'Units', 'Inches', 'Position', [0, 0, 4.5, 7.5], 'PaperUnits', 'Inches', 'PaperSize', [4.5, 7.5]);
 saveas(f, fname, 'jpeg');
 
 end
